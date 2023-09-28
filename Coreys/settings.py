445537fbd1a6b8b6b6097d05f89ecb7d929a94a0
Aryan Anubhav Singh <116826634+Aryn1204@users.noreply.github.com>
2023-09-28 22:23:28 +0530

    gunicorn

--- Coreys/settings.py
+++ Coreys/settings.py
@@ -48,9 +48,9 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
